331ba6f87100e4643a2b52e5d59bbd577ace8746
Ilya <sii98@list.ru>
2018-03-15 16:04:06 +0300

    task13

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function count(s, pairs) {
-  // your implementation
+  var N = 1;
+  var result = 0;
+  var test = "";
+  for (var i = 0; i < pairs.length; i++) {
+    N *= Math.pow(pairs[i][0], pairs[i][1]);
+    if (N > 100000000) return 0;
+  }
+  for (var k = 0; k < N; k++) {
+    for (var j = 0; j < s.length; j++) {
+      if (nod(k + j, N) == 1) {
+        test +=  "1";
+      } else {
+        test +=  "0";
+      }
+    }
+  }
+  var pos = 0;
+  while (pos < test.length) {
+    var foundPos = test.indexOf(s, pos);
+    if (foundPos == -1) {
+      break;
+    } else {
+      result++;
+    }
+    pos = foundPos + 1;
+  }
+  return result % 1000000007;
+}
+
+function nod(a, b) {
+  if (b == 0) return a;
+  return nod(b, a % b);
 }
